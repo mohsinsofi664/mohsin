06606cbceec504d9ea78e86436eb4fcb20f802c6
mohsinsofi664 <sofimohsin2003@gmail.com>
2024-11-09 19:18:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+main_token = '7928619073:AAFOhfbqmkPR4ZZ7RIW9Rjafbtqy-D5qZ6U'
+
+monitor_token = '7263160696:AAGDk26njy7aufeGBDeVikR8GimWBicHJD0'
+
+segmind_api_key = "SG_43064ae3acd773a6"
+
+chat_id = "5803180946" 
